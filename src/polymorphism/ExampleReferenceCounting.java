9a86bacc197e7f7c0e9881fb7dfc30e21064e61e
lbuthman <lbuthman@gmail.com>
2017-05-16 19:22:27 -0500

    Add reference counting example

---
+++ src/polymorphism/ExampleReferenceCounting.java
@@ -0,0 +1,64 @@
+package src.polymorphism;
+
+/**
+ * Created by lbuthman on 5/16/17.
+ */
+
+class Shared {
+    private  int refcount = 0;
+    private static long counter = 0;
+    private final long id = counter++;
+
+    public Shared() {
+        System.out.println("Creating " + this);
+    }
+
+    public void addRef() {
+        refcount++;
+    }
+
+    protected void dispose() {
+        if (--refcount == 0) {
+            System.out.println("Disposing " + this);
+        }
+    }
+
+    public String toString() {
+        return "Shared " + id;
+    }
+}
+
+class Composing {
+    private Shared shared;
+    private static long counter = 0;
+    private final long id = counter++;
+
+    public Composing(Shared shared) {
+        System.out.println("Creating " + this);
+        System.out.println("Id is " + id);
+        System.out.println("Counter is " + counter);
+        this.shared = shared;
+        this.shared.addRef();
+    }
+
+    protected void dispose() {
+        System.out.println("Disposing " + this);
+        shared.dispose();
+    }
+
+    public String toString() {
+        return "Composing " + id;
+    }
+}
+
+public class ExampleReferenceCounting {
+    public static void main(String[] args) {
+        Shared shared = new Shared();
+        Composing[] composings = {new Composing(shared), new Composing(shared), new Composing(shared),
+                new Composing(shared), new Composing(shared)};
+        for (Composing c: composings) {
+            c.dispose();
+        }
+    }
+
+}
