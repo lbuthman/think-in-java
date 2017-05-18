d3263d489f2f5681ed31740683db4e324fffe259
lbuthman <lbuthman@gmail.com>
2017-05-17 20:05:18 -0500

    Add Ex3 reviewing initialization concepts again

---
+++ src/interfaces/Ex3.java
@@ -0,0 +1,29 @@
+package src.interfaces;
+
+/**
+ * Created by lbuthman on 5/17/17.
+ */
+
+abstract class Number {
+    abstract void print();
+
+    public Number() {
+        print();
+    }
+}
+
+class MyNumber extends Number {
+    int i = 11;
+    void print() {
+        System.out.println(i);
+    }
+}
+
+public class Ex3 {
+    public static void main(String[] args) {
+        MyNumber myNumber = new MyNumber();
+        myNumber.print();
+        //prints 0 when initially called by parent constructor during initialization
+        //prints 11 when called from derived class
+    }
+}
