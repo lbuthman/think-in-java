87601fc7febdd480815ac7cd944a64632a1cc959
lbuthman <lbuthman@gmail.com>
2017-05-20 11:42:46 -0500

    Update for Ex3, private outer field, displayed via toString from inner class

--- src/innerclasses/Ex1Outer.java
+++ src/innerclasses/Ex1Outer.java
@@ -5,12 +5,21 @@ package src.innerclasses;
  */
 public class Ex1Outer {
 
+    private String member;
+
     //public constructor
-    public Ex1Outer() {System.out.println("Sorry, you are on the outside");}
+    public Ex1Outer(String s) {
+        this.member = s;
+        System.out.println("Sorry, you are on the outside");
+    }
 
     class Inner {
         //public constructor
         public Inner() {System.out.println("Welcome to the inner circle.");}
+
+        public String toString() {
+            return member;
+        }
     }
 
     //return reference to Inner class from Outer
@@ -19,7 +28,8 @@ public class Ex1Outer {
     }
 
     public static void main(String[] args) {
-        Ex1Outer outer = new Ex1Outer();
+        Ex1Outer outer = new Ex1Outer("trial basis");
         Ex1Outer.Inner inner = outer.getInside();
+        System.out.println("Your membership status is " + inner.toString());
     }
 }
