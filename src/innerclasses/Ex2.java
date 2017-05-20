b59e836d11409040f8ccc92b1559ef2cfa639249
lbuthman <lbuthman@gmail.com>
2017-05-20 11:39:44 -0500

    Add Ex2, inner class access private fields of outer class

---
+++ src/innerclasses/Ex2.java
@@ -0,0 +1,71 @@
+package src.innerclasses;
+
+/**
+ * Created by lbuthman on 5/20/17.
+ */
+
+interface Selector {
+    boolean end();
+    String current();
+    void next();
+}
+
+public class Ex2 {
+
+    private String[] words;
+    private int next = 0;
+
+    public Ex2(int size) { words = new String[size]; }
+
+    public void add(String s) {
+        if (next < words.length) {
+            words[next++] = s;
+        }
+    }
+
+    public String toString() {
+
+        String wordString = "";
+
+        for (String s: words) {
+            wordString += s;
+        }
+
+        return wordString;
+    }
+
+    private class StringSelector implements Selector {
+
+        private int i = 0;
+
+        public boolean end() { return i == words.length; }
+
+        public String current() { return words[i]; }
+
+        public void next() {
+            if (i < words.length) {
+                i++;
+            }
+        }
+    }
+
+    public Selector selector() {
+        return new StringSelector();
+    }
+
+    public static void main(String[] args) {
+        Ex2 ex2 = new Ex2(11);
+
+        for (int i=0; i<11; i++) {
+            ex2.add(Integer.toString(i));
+        }
+
+        Selector selector = ex2.selector();
+
+        while (!selector.end()) {
+            System.out.println(selector.current() + " " + ex2.toString());
+            selector.next();
+        }
+
+    }
+}
