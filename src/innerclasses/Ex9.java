e9bfe63361582ad012b918d03da66e59808d1baa
lbuthman <lbuthman@gmail.com>
2017-05-20 18:15:43 -0500

    Add Ex10, inner class implements interface conditionally inside scope of method of outer class

--- src/innerclasses/Ex9.java
+++ src/innerclasses/Ex9.java
@@ -9,22 +9,35 @@ interface Shoe {
 }
 
 class Foot {
-    public Shoe prepare() {
-        class Gear implements Shoe {
-            public void tie() {
-                System.out.println("Shoe geared tied on foot.");
+    public Shoe prepare(boolean ready) {
+        if (ready) {
+            class Gear implements Shoe {
+                public void tie() {
+                    System.out.println("Shoe geared tied on foot.");
 
+                }
             }
+            return new Gear();
+        } else {
+            class Gear implements Shoe {
+                public void tie() {
+                    System.out.println("Hurry up! We need to go!");
+
+                }
+            }
+            return new Gear();
         }
 
-        return new Gear();
     }
 }
 
 public class Ex9 {
     public static void main(String[] args) {
         Foot foot = new Foot();
-        Shoe shoe = foot.prepare();
+        Boolean ready = true;
+        Shoe shoe = foot.prepare(ready);
         shoe.tie();
+        Shoe noShoe = foot.prepare(!ready);
+        noShoe.tie();
     }
 }
