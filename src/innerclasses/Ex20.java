854704b151e8198edfaaec134f13a0c4705a071d
lbuthman <lbuthman@gmail.com>
2017-05-21 08:33:12 -0500

    Add Ex20, inner class inside interface that implements that interface

---
+++ src/innerclasses/Ex20.java
@@ -0,0 +1,21 @@
+package src.innerclasses;
+
+/**
+ * Created by lbuthman on 5/21/17.
+ */
+
+interface Positive {
+    void shine();
+    class Negative implements Positive {
+        public void shine() {
+            System.out.println("A slightly dimmed shine, beautiful and dark.");
+        }
+        public static void main(String[] args) {
+            new Negative().shine();
+        }
+    }
+}
+
+public class Ex20 {
+
+}
