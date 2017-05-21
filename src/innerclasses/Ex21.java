51dd0f02331f7dff2beaec4723e1df49c2764738
lbuthman <lbuthman@gmail.com>
2017-05-21 08:42:27 -0500

    Add Ex21, inner class contains static method that calls methods of interface; implementation of interface gets passed to static method of inner class which calls the implemented methods

---
+++ src/innerclasses/Ex21.java
@@ -0,0 +1,27 @@
+package src.innerclasses;
+
+/**
+ * Created by lbuthman on 5/21/17.
+ */
+
+interface Drink {
+    void stir();
+    void sip();
+    class Martini {
+
+        public static void enjoy(Drink drink) {
+            drink.stir();
+            drink.sip();
+        }
+    }
+}
+
+public class Ex21 implements Drink {
+    public void stir() {System.out.println("Please shake, never stir.");}
+    public void sip() {System.out.println("Fancy sip.");}
+    public static void main(String[] args) {
+
+        Martini.enjoy(new Ex21());
+
+    }
+}
