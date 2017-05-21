f565d631bcc3f434bb0c6521c09b6ba92aa82bb5
lbuthman <lbuthman@gmail.com>
2017-05-21 06:13:28 -0500

    Add Ex12, refactor as anonymous inner class

--- src/innerclasses/Ex7.java
+++ src/innerclasses/Ex7.java
@@ -3,6 +3,11 @@ package src.innerclasses;
 /**
  * Created by lbuthman on 5/20/17.
  */
+
+interface LoudMouth {
+    void letItSlip();
+}
+
 public class Ex7 {
 
     private String secret = "I'm a secret to all.";
@@ -11,23 +16,21 @@ public class Ex7 {
         System.out.println("psspssppsppsppspss");
     }
 
-    public class LoudMouth {
-
-        public void letItSlip() {
-            whisper();
-            secret = "Oscar was lying about being sick.";
-        }
-    }
-
     public LoudMouth investigate() {
-        return new LoudMouth();
+
+        return new LoudMouth() {
+            public void letItSlip() {
+                whisper();
+                secret = "Oscar was lying about being sick.";
+            }
+        };
     }
 
     public static void main(String[] args) {
         Ex7 ex7 = new Ex7();
         System.out.println(ex7.secret);
-        LoudMouth lm = ex7.investigate();
-        lm.letItSlip();
+        LoudMouth blab = ex7.investigate();
+        blab.letItSlip();
         System.out.println(ex7.secret);
     }
 }
