27802561266fada5d90dffba0d32fd2e10da2a69
lbuthman <lbuthman@gmail.com>
2017-05-21 11:50:13 -0500

    Add Ex2, Set and TreeSet example

---
+++ src/holdingyourobjects/Ex2.java
@@ -0,0 +1,16 @@
+package src.holdingyourobjects;
+
+import java.util.*;
+
+/**
+ * Created by lbuthman on 5/21/17.
+ */
+public class Ex2 {
+    public static void main(String[] args) {
+        Set<Integer> c = new TreeSet<Integer>() {};
+        for (int i=0; i<10; i++) { c.add(i); }
+        for (Integer n: c) {
+            System.out.print(n + ", ");
+        }
+    }
+}
