543d28df629753e940632879a5205d1bf3fc929e
lbuthman <lbuthman@gmail.com>
2017-05-23 06:16:24 -0500

    Add Ex16, iterate over set to find total vowels

---
+++ src/holdingyourobjects/Ex16.java
@@ -0,0 +1,40 @@
+package src.holdingyourobjects;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+/**
+ * Created by lbuthman on 5/23/17.
+ */
+public class Ex16 {
+    public static void main(String[] args) {
+
+        Set<String> vowelSet = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+        String vowels = "a e i o u";
+        Collections.addAll(vowelSet, vowels.split(" "));
+
+        ArrayList<String> wordSet = new ArrayList<>();
+        String text = "Hi, my name is Bingo, I like to climb things. " +
+                "Can I have a banana please? Eek eek. I'm a ninja, it's " +
+                "my birthday, I'm a ninja.";
+        Collections.addAll(wordSet, text.split(" "));
+
+        int totalVowels = 0;
+
+        for (String word: wordSet) {
+            int localVowels = 0;
+            for (String vowel: vowelSet) {
+                for (int i=0; i<word.length(); i++) {
+                    if (word.substring(i, i + 1).toLowerCase().contentEquals(vowel)) {
+                        localVowels++;
+                        totalVowels++;
+                    }
+                }
+            }
+            System.out.println("Word " + word + " has " + localVowels + " vowels.");
+        }
+
+        System.out.println("There are " + totalVowels + " vowels present.");
+
+    }
+}
